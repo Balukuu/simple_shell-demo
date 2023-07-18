0fb22023799e4eeb10cd49946b0ca6eb2ee84fd3
Edgar Baluku <edgarbaluku@gmail.com>
2023-07-18 19:16:57 +0300

    added the wait process

--- execve.c
+++ execve.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(void)
+
 {
+	pid_t  pid;
+
         char *argv[] = {"/bin/ls", "-l", NULL};
-	
-	int val = execve(argv[0],argv, NULL);
+	pid =fork();
 
-	if (val == -1) 
-		perror("error");
+	if(pid == -1)
+		return -1;
+        
+	if (pid == 0)
+	{
+		int val = execve(argv[0],argv, NULL);
 
-	printf("done execve\n");
+	        if (val == -1) 
+		     perror("error");
+	}
+	else 
+	{
+		wait(NULL);
+
+	        printf("done execve\n");
+	}
 
          return 0;
 
