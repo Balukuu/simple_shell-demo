a7e051b4547de7b78200b38cc00f0c4be3f72ea0
Edgar Baluku <edgarbaluku@gmail.com>
2023-07-18 07:04:44 +0300

    the wait system call

---
+++ wait.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+int main(void)
+{
+       pid_t pid;
+       pid =fork();
+
+       if (pid == -1)
+       {
+            perror("unsuccessful\n");
+	    return 1;
+       }
+     
+       if (pid == 0)
+       {
+       printf ("child process\n");
+       }
+
+       else
+       {
+       printf ("parent process\n");
+       }
+
+       return 0;
+}
+
